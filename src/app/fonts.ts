cbd3842051035b9a87c934737650153bb3e32e1c
hhhssh <hanshermawan1@gmail.com>
2024-12-18 01:18:07 -0800

    add file

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+
+export const Tusker5700 = localFont({
+  src: "../../public/fonts/TuskerGrotesk-5700Bold.ttf",
+  weight: "5700",
+  style: "normal",
+  display: "swap",
+});
+
+export const Tusker6700 = localFont({
+  src: "../../public/fonts/TuskerGrotesk-6700Bold.ttf",
+  weight: "6700",
+  style: "normal",
+  display: "swap",
+});
