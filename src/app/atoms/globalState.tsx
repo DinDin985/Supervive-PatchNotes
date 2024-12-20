ae17f4969da6dad6ae295fd1577bdc4cd6e4194a
hhhssh <hanshermawan1@gmail.com>
2024-12-19 19:44:51 -0800

    fanwarning fix

--- src/app/atoms/globalState.tsx
+++ src/app/atoms/globalState.tsx
@@ -1,7 +1,7 @@
 import { atom } from "jotai";
 
 const toggleMenuAtom = atom<boolean>(false);
-const toggleFanWarningAtom = atom<boolean>(false);
+const toggleFanWarningAtom = atom<boolean>(true);
 const setVisibleSectionAtom = atom<string>("Summary");
 const visablePatchNotesPostsAtom = atom<number>(6);
 
