c37ed12139853233b851fdb7ddc478eacb438a9f
hhhssh <hanshermawan1@gmail.com>
2025-01-22 12:15:13 -0800

    Temp navbar change

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { WorldLanguageDropDownMenu } from "../WorldLanguageDropDownMenu";
 import NavbarBackground from "./NavbarBackground";
 import NavbarLinks from "./NavbarLinks";
@@ -7,7 +6,7 @@ import NavbarSidemenuToggle from "./NavbarSidemenuToggle";
 
 export default function Navbar() {
   return (
-    <header className="nav-header fixed left-0 top-0 z-[9999] h-24 w-full overflow-hidden bg-transparent">
+    <header className="nav-header fixed left-0 top-0 z-[9999] h-24 w-full bg-transparent">
       <NavbarBackground />
 
       <div className="relative z-20 flex h-full justify-between bg-left-bottom px-2 xs:px-4 md:px-8">
@@ -19,14 +18,6 @@ export default function Navbar() {
           <NavbarShineLinks />
         </aside>
       </div>
-
-      <Image
-        src={"/nav-bar/header-sidemenu.png"}
-        className="absolute left-[19.5rem] top-40 z-10 max-h-24 rotate-180 scale-[6]"
-        alt="Background Image"
-        height={500}
-        width={500}
-      />
     </header>
   );
 }
